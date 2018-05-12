c41a7a1195ad56d14c45147c53195cef3d9ea18a
Jongkeon <30586378+whdrjs@users.noreply.github.com>
2018-05-12 14:20:05 +0900

    ver 1.0

---
+++ app/src/main/java/com/namjongbin/fridge_angel/CharacterFragment.java
@@ -0,0 +1,42 @@
+package com.namjongbin.fridge_angel;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.felipecsl.gifimageview.library.GifImageView;
+
+public class CharacterFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+
+
+    public CharacterFragment() {
+        // Required empty public constructor
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_character,container,false);
+        container.removeAllViews();
+
+        ImageView image =(ImageView)rootView.findViewById(R.id.imageView);
+
+        GlideDrawableImageViewTarget charimg=new GlideDrawableImageViewTarget(image);
+        Glide.with(this).load(R.drawable.sd_charging).into(charimg);
+
+        return rootView;
+    }
+
+}
---
+++ app/src/main/java/com/namjongbin/fridge_angel/SettingsScreen.java
@@ -0,0 +1,27 @@
+package com.namjongbin.fridge_angel;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsScreen extends PreferenceFragment{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings_screen);
+
+        SwitchPreference switchSet=(SwitchPreference)findPreference("switch");
+        switchSet.setEnabled(true);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        view.setBackgroundColor(getResources().getColor(android.R.color.white));
+        container.removeAllViews();
+        return view;
+    }
+}
