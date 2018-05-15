42f7905adb7e44dd345b5af8a551a61ee899ac1b
seok_bin <jj9243@naver.com>
2018-05-15 22:57:33 +0900

    aa

--- app/src/test/java/com/namjongbin/fridge_angel/ExampleUnitTest.java
+++ app/src/test/java/com/namjongbin/fridge_angel/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.namjongbin.fridge_angel;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
