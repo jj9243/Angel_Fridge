aafd78c0a5dbba03713e209fd949553281000368
whdrjs <jkeon72@gmail.com>
2018-05-15 23:29:11 +0900

    ver 1.0.3.1 list activity

--- app/src/main/java/com/namjongbin/fridge_angel/HomeFragment.java
+++ app/src/main/java/com/namjongbin/fridge_angel/HomeFragment.java
@@ -2,41 +2,24 @@ package com.namjongbin.fridge_angel;
 
 //import android.app.Fragment;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TableLayout;
-import android.widget.Toast;
+
 
 public class HomeFragment extends Fragment {
-//    MainActivity main;
-//    OnFragmentOpen mOpen;
-//
-//    public interface OnFragmentOpen{
-//        public void onSentCode(int code);
-//    }
-//
-//    public HomeFragment(){
-//    }
-//
-//    public void onAttach(Activity activity){
-//        super.onAttach(activity);
-//        try{
-//            mOpen=(OnFragmentOpen)activity;
-//        }catch (ClassCastException e){
-//            throw new ClassCastException(activity.toString()+" must implent OnFragmentOpen");
-//        }
-//    }
+
 
     @Nullable
     @Override
-
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
+        final View rootView = (View) inflater.inflate(R.layout.fragment_home, container, false);
 
         container.removeAllViews();
 
@@ -44,13 +27,11 @@ public class HomeFragment extends Fragment {
         table.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                ListsFragment listFragment=new ListsFragment();
-                FragmentManager manager=getFragmentManager();
-                manager.beginTransaction().replace(R.id.frame_fragment, listFragment, listFragment.getTag()).commit();
+               Intent intent=new Intent(getActivity().getApplicationContext(),ListActivity.class);
+               startActivity(intent);
+
             }
         });
-
-
         return rootView;
     }
 }
---
+++ app/src/main/java/com/namjongbin/fridge_angel/ListViewAdapter.java
@@ -0,0 +1,86 @@
+package com.namjongbin.fridge_angel;
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class ListViewAdapter extends BaseAdapter {
+
+    private ArrayList<ListVO> listVO = new ArrayList<ListVO>() ;
+    public ListViewAdapter() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return listVO.size() ;
+    }
+
+    // ** 이 부분에서 리스트뷰에 데이터를 넣어줌 **
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //postion = ListView의 위치      /   첫번째면 position = 0
+        final int pos = position;
+        final Context context = parent.getContext();
+
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.list_item, parent, false);
+        }
+
+        ImageView image = (ImageView) convertView.findViewById(R.id.img) ;
+        TextView title = (TextView) convertView.findViewById(R.id.title) ;
+        TextView Context = (TextView) convertView.findViewById(R.id.context) ;
+
+
+        ListVO listViewItem = listVO.get(position);
+
+        // 아이템 내 각 위젯에 데이터 반영
+        image.setImageDrawable(listViewItem.getImg());
+        title.setText(listViewItem.getTitle());
+        Context.setText(listViewItem.getContext());
+
+
+        //리스트뷰 클릭 이벤트
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, (pos+1)+"번째 리스트가 클릭되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return convertView;
+    }
+
+
+    @Override
+    public long getItemId(int position) {
+        return position ;
+    }
+
+
+    @Override
+    public Object getItem(int position) {
+        return listVO.get(position) ;
+    }
+
+    // 데이터값 넣어줌
+    public void addVO(Drawable icon, String title, String desc) {
+        ListVO item = new ListVO();
+
+        item.setImg(icon);
+        item.setTitle(title);
+        item.setContext(desc);
+
+        listVO.add(item);
+    }
+}
