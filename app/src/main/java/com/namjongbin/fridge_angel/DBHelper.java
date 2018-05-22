e484b5db97e79069035b8d422cf720131c183045
jj9243 <32669294+jj9243@users.noreply.github.com>
2018-05-22 23:02:33 +0900

    Update DBHelper.java

--- app/src/main/java/com/namjongbin/fridge_angel/DBHelper.java
+++ app/src/main/java/com/namjongbin/fridge_angel/DBHelper.java
@@ -87,7 +87,7 @@ public class DBHelper extends SQLiteOpenHelper {
         String result = "";
 
         // DB에 있는 데이터를 쉽게 처리하기 위해 Cursor를 사용하여 테이블에 있는 모든 데이터 출력
-        Cursor cursor = db.rawQuery("SELECT * FROM ITEM", null);
+        Cursor cursor = db.rawQuery("SELECT * FROM ITEM ORDER BY year ASC,month ASC,day ASC;", null);
         while (cursor.moveToNext()) {
             result += cursor.getString(0)
                     +" "
