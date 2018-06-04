7ff62ce109cdef70e1bb7ecf48ef7a86cdf2c61e
jj9243 <32669294+jj9243@users.noreply.github.com>
2018-06-04 15:18:53 +0900

    Update CharacterFragment.java

--- app/src/main/java/com/namjongbin/fridge_angel/CharacterFragment.java
+++ app/src/main/java/com/namjongbin/fridge_angel/CharacterFragment.java
@@ -1,5 +1,6 @@
 package com.namjongbin.fridge_angel;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
@@ -15,6 +16,10 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.felipecsl.gifimageview.library.GifImageView;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class CharacterFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +27,8 @@ public class CharacterFragment extends Fragment {
     private static final String ARG_PARAM2 = "param2";
 
     int healthy;
+    int year,month,day;
+    int expiredCount = 0;
     Boolean touchflag=false;
     // TODO: Rename and change types of parameters
 
@@ -33,11 +40,23 @@ public class CharacterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_character,container,false);
         container.removeAllViews();
         healthy=100;
         ImageView image =(ImageView)rootView.findViewById(R.id.imageView);
         ProgressBar progress=rootView.findViewById(R.id.progressBar);
+
+        int expireCount = this.getExpiredCount(getContext());
+        if(expireCount <= 2){
+            // 슈다 기분 좋음
+        }
+        else if(expireCount > 2 && expireCount <= 6){
+            // 슈다 기분 보통
+        }
+        else{
+            // 슈다 화남
+        }
 
         final GlideDrawableImageViewTarget cd=new GlideDrawableImageViewTarget(image);
 
@@ -51,6 +70,7 @@ public class CharacterFragment extends Fragment {
                 Glide.with(getContext()).load(R.drawable.cdtouch).into(cd);
             }
         });
+
 //
 //        if(progress !=null){
 //            progress.setVisibility(View.VISIBLE);
@@ -62,5 +82,57 @@ public class CharacterFragment extends Fragment {
         //progress.getProgressDrawable().setColorFilter(Color.rgb(10,210,200), PorterDuff.Mode.MULTIPLY);
         return rootView;
     }
+
+    public int getExpiredCount(Context context){
+        int count = 0;
+        Calendar cal = Calendar.getInstance();
+        int yearToday = cal.get(cal.YEAR);
+        int monthToday = cal.get(cal.MONTH) + 1;
+        int dayToday = cal.get(cal.DATE);
+        final DBHelper db = new DBHelper(context,"ITEM.db",null,2);
+        String[] foodItem = db.getResult().split("\n");
+        String[] Title = new String[db.columnNum()];
+        String[] Context = new String[db.columnNum()];
+        for(int i = 0 ; i < db.columnNum(); i++) {
+            String[] str = new String[2];
+            str = foodItem[i].split(":");
+            Title[i] = str[0].replaceAll("[0-9]", "");
+            Context[i] = str[1];
+            parseItem(Context[i]);
+
+            if(year < yearToday)
+                count++;
+            else if(month < monthToday)
+                count++;
+            else if(day < dayToday)
+                count++;
+
+        }
+
+        return count;
+    }
+    public void parseItem(String item){
+
+        Date today = new Date();
+        SimpleDateFormat date = new SimpleDateFormat("yyyy년MM월dd일");
+        String d = date.format(today).toString();
+
+        if(item.contains("년")) {
+            year = Integer.parseInt(item.substring(item.indexOf('년') - 4, item.indexOf('년')));
+
+        }
+        else
+            year = Integer.parseInt(d.substring(d.indexOf('년')-4,d.indexOf('년')));
+        if(item.contains("월"))
+            month = Integer.parseInt(item.substring(item.indexOf('월')-2,item.indexOf('월')).trim());
+        else
+            month = Integer.parseInt(d.substring(d.indexOf('월')-2,d.indexOf('월')));
+        if(item.contains("일"))
+            day = Integer.parseInt(item.substring(item.indexOf('일')-2,item.indexOf('일')).trim());
+        else
+            day = Integer.parseInt(d.substring(d.indexOf('일')-2,d.indexOf('일')));
+
+    }
+
 
 }
