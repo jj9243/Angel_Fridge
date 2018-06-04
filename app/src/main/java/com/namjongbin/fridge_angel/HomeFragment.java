b2d7680fc4c8324eedf9c9cf4126e9f161a60a0c
Namkangwoo <rkddn88@naver.com>
2018-06-04 20:26:39 +0900

    add calendar(test) in home fragment

--- app/src/main/java/com/namjongbin/fridge_angel/HomeFragment.java
+++ app/src/main/java/com/namjongbin/fridge_angel/HomeFragment.java
@@ -6,8 +6,10 @@ package com.namjongbin.fridge_angel;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +22,12 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -27,13 +35,16 @@ import java.util.Date;
 
 
 public class HomeFragment extends Fragment {
-    TextView itemText,ddayText,dateText;
+    TextView itemText, ddayText, dateText;
     TableLayout table;
-    String Title="" ;
-    String Context="";
-    int dday,count = 0;
+    String Title = "";
+    String Context = "";
+    int dday, count = 0;
     int year, month, day;
-
+    Calendar calendar = Calendar.getInstance();
+    int y = -1;
+    int m = -1;
+    int d = -1;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -41,33 +52,56 @@ public class HomeFragment extends Fragment {
 
         container.removeAllViews();
 
-        table= (TableLayout) rootView.findViewById(R.id.table);
-        Button youtubeButton = (Button)rootView.findViewById(R.id.recipeBtn);
-        Button eatButton=rootView.findViewById(R.id.eatBtn);
+        MaterialCalendarView materialCalendarView = (MaterialCalendarView)rootView.findViewById(R.id.cal);
 
-        itemText = (TextView)rootView.findViewById(R.id.item);
-        dateText = (TextView)rootView.findViewById(R.id.date);
-        ddayText = (TextView)rootView.findViewById(R.id.dday);
+        //Toast.makeText(getApplicationContext(),""+calendar.get(Calendar.YEAR)+" "+calendar.get(Calendar.MONTH)+" "+calendar.get(Calendar.DAY_OF_MONTH),Toast.LENGTH_LONG).show();
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.SUNDAY)
+                .setMinimumDate(CalendarDay.from(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)))
+                .setMaximumDate(CalendarDay.from(2019, 12, 31))
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+
+        materialCalendarView.addDecorators(
+                new HomeFragment.SundayDecorator(),
+                new HomeFragment.SaturdayDecorator());
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                Toast.makeText(getActivity().getApplicationContext(), "" + date.getYear() + "년 " + (date.getMonth() + 1) + "월 " + date.getDay() + "일 입니다.", Toast.LENGTH_LONG).show();
+                y = date.getYear();
+                m = date.getMonth();
+                d = date.getDay();
+            }
+        });
+
+        table = (TableLayout) rootView.findViewById(R.id.table);
+        Button youtubeButton = (Button) rootView.findViewById(R.id.recipeBtn);
+        Button eatButton = rootView.findViewById(R.id.eatBtn);
+
+        itemText = (TextView) rootView.findViewById(R.id.item);
+        dateText = (TextView) rootView.findViewById(R.id.date);
+        ddayText = (TextView) rootView.findViewById(R.id.dday);
 
         parseDate();
-        if(dday<0) {
+        if (dday < 0) {
             ddayText.setText("D-DAY");
             table.setBackgroundResource(R.drawable.cardr);
             eatButton.setText("버렸어요 :(");
-        }else if(dday==0) {
+        } else if (dday == 0) {
             ddayText.setText("D-DAY");
-        }
-        else {
+        } else {
             ddayText.setText("D-" + dday);
         }
 
         itemText.setText(Title);
-        dateText.setText(Context+" 까지");
+        dateText.setText(Context + " 까지");
 
         youtubeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
 
-                String fname=itemText.getText().toString()+" "+"레시피";
+                String fname = itemText.getText().toString() + " " + "레시피";
 
                 Intent intent = new Intent(Intent.ACTION_SEARCH);
                 intent.setPackage("com.google.android.youtube");
@@ -85,15 +119,15 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 eatFood();
-                Toast.makeText(getActivity(),"Eat: "+Title,Toast.LENGTH_LONG).show();
+                Toast.makeText(getActivity(), "Eat: " + Title, Toast.LENGTH_LONG).show();
             }
         });
 
         table.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               Intent intent=new Intent(getActivity().getApplicationContext(),ListActivity.class);
-               startActivity(intent);
+                Intent intent = new Intent(getActivity().getApplicationContext(), ListActivity.class);
+                startActivity(intent);
 
             }
         });
@@ -105,25 +139,27 @@ public class HomeFragment extends Fragment {
 
         return rootView;
     }
-    public void parseDate(){
-        final DBHelper db = new DBHelper(getActivity(),"ITEM.db",null,2);
-        if(db.getResult().equals("") || db.getResult() == null)
+
+    public void parseDate() {
+        final DBHelper db = new DBHelper(getActivity(), "ITEM.db", null, 2);
+        if (db.getResult().equals("") || db.getResult() == null)
             return;
         String[] foodItem = db.getResult().split("\n");
         String[] str = new String[2];
-        for(int i = 0 ; i < 1; i++) {
-            String temp="";
+        for (int i = 0; i < 1; i++) {
+            String temp = "";
             str = foodItem[i].split(":");
             Title = str[0].replaceAll("[0-9]", "");
             Context = str[1];
         }
 
-        int year = Integer.parseInt(Context.substring(Context.indexOf("년")-4,Context.indexOf("년")));
-        int month = Integer.parseInt(Context.substring(Context.indexOf("월")-2,Context.indexOf("월")).trim());
-        int day = Integer.parseInt(Context.substring(Context.indexOf("일")-2,Context.indexOf("일")).trim());
+        int year = Integer.parseInt(Context.substring(Context.indexOf("년") - 4, Context.indexOf("년")));
+        int month = Integer.parseInt(Context.substring(Context.indexOf("월") - 2, Context.indexOf("월")).trim());
+        int day = Integer.parseInt(Context.substring(Context.indexOf("일") - 2, Context.indexOf("일")).trim());
 
-         dday = countdday(year,month,day);
+        dday = countdday(year, month, day);
     }
+
     public int countdday(int myear, int mmonth, int mday) {
         try {
             SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
@@ -132,54 +168,91 @@ public class HomeFragment extends Fragment {
             Calendar ddayCal = Calendar.getInstance(); //오늘날자를 가져와 변경시킴
 
             mmonth -= 1; // 받아온날자에서 -1을 해줘야함.
-            ddayCal.set(myear,mmonth,mday);// D-day의 날짜를 입력
-            Log.e("테스트",simpleDateFormat.format(todaCal.getTime()) + "");
-            Log.e("테스트",simpleDateFormat.format(ddayCal.getTime()) + "");
+            ddayCal.set(myear, mmonth, mday);// D-day의 날짜를 입력
+            Log.e("테스트", simpleDateFormat.format(todaCal.getTime()) + "");
+            Log.e("테스트", simpleDateFormat.format(ddayCal.getTime()) + "");
 
-            long today = todaCal.getTimeInMillis()/86400000; //->(24 * 60 * 60 * 1000) 24시간 60분 60초 * (ms초->초 변환 1000)
-            long dday = ddayCal.getTimeInMillis()/86400000;
+            long today = todaCal.getTimeInMillis() / 86400000; //->(24 * 60 * 60 * 1000) 24시간 60분 60초 * (ms초->초 변환 1000)
+            long dday = ddayCal.getTimeInMillis() / 86400000;
             long count = dday - today; // 오늘 날짜에서 dday 날짜를 빼주게 됩니다.
             return (int) count;
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             e.printStackTrace();
             return -1;
         }
     }
 
-    public void eatFood(){
-        final DBHelper db = new DBHelper(getActivity(),"ITEM.db",null,2);
-        if(db.getResult().equals("") || db.getResult() == null)
+    public void eatFood() {
+        final DBHelper db = new DBHelper(getActivity(), "ITEM.db", null, 2);
+        if (db.getResult().equals("") || db.getResult() == null)
             return;
 
         String deleteItem = Title;
         String deleteDate = Context;
         parseItem(deleteDate);
-        db.delete(db.getId(deleteItem.trim(),year,month,day));
+        db.delete(db.getId(deleteItem.trim(), year, month, day));
 
-        Intent intent = new Intent(getActivity(),ListActivity.class);
+        Intent intent = new Intent(getActivity(), ListActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
     }
 
-    public void parseItem(String item){
+    public void parseItem(String item) {
 
         Date today = new Date();
         SimpleDateFormat date = new SimpleDateFormat("yyyy년MM월dd일");
         String d = date.format(today).toString();
 
-        if(item.contains("년")) {
+        if (item.contains("년")) {
             year = Integer.parseInt(item.substring(item.indexOf('년') - 4, item.indexOf('년')));
+        } else
+            year = Integer.parseInt(d.substring(d.indexOf('년') - 4, d.indexOf('년')));
+        if (item.contains("월"))
+            month = Integer.parseInt(item.substring(item.indexOf('월') - 2, item.indexOf('월')).trim());
+        else
+            month = Integer.parseInt(d.substring(d.indexOf('월') - 2, d.indexOf('월')));
+        if (item.contains("일"))
+            day = Integer.parseInt(item.substring(item.indexOf('일') - 2, item.indexOf('일')).trim());
+        else
+            day = Integer.parseInt(d.substring(d.indexOf('일') - 2, d.indexOf('일')));
+    }
+
+    public class SaturdayDecorator implements DayViewDecorator {
+
+        private final Calendar calendar = Calendar.getInstance();
+
+        public SaturdayDecorator() {
         }
-        else
-            year = Integer.parseInt(d.substring(d.indexOf('년')-4,d.indexOf('년')));
-        if(item.contains("월"))
-            month = Integer.parseInt(item.substring(item.indexOf('월')-2,item.indexOf('월')).trim());
-        else
-            month = Integer.parseInt(d.substring(d.indexOf('월')-2,d.indexOf('월')));
-        if(item.contains("일"))
-            day = Integer.parseInt(item.substring(item.indexOf('일')-2,item.indexOf('일')).trim());
-        else
-            day = Integer.parseInt(d.substring(d.indexOf('일')-2,d.indexOf('일')));
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            day.copyTo(calendar);
+            int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+            return weekDay == Calendar.SATURDAY;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new ForegroundColorSpan(getResources().getColor(R.color.color5)));
+        }
+    }
+
+    public class SundayDecorator implements DayViewDecorator {
+
+        private final Calendar calendar = Calendar.getInstance();
+
+        public SundayDecorator() {
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            day.copyTo(calendar);
+            int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
+            return weekDay == Calendar.SUNDAY;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.addSpan(new ForegroundColorSpan(getResources().getColor(R.color.highLight)));
+        }
     }
 }
