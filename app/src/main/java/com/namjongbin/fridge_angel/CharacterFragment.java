0d790c7d4ae74363d0f91e4f99084abbb163473f
whdrjs <jkeon72@gmail.com>
2018-06-06 17:42:20 +0900

    ver 1.2.9 맘대로 건들지마라

--- app/src/main/java/com/namjongbin/fridge_angel/CharacterFragment.java
+++ app/src/main/java/com/namjongbin/fridge_angel/CharacterFragment.java
@@ -1,6 +1,7 @@
 package com.namjongbin.fridge_angel;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
@@ -26,10 +27,11 @@ public class CharacterFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    public SharedPreferences prefs;
     int healthy;
-    int year,month,day;
+    int year, month, day;
     int expiredCount = 0;
-    Boolean touchflag=false;
+    Boolean touchflag = false;
     // TODO: Rename and change types of parameters
 
 
@@ -41,27 +43,32 @@ public class CharacterFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_character,container,false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_character, container, false);
         container.removeAllViews();
-        healthy=100;
-        ImageView image =(ImageView)rootView.findViewById(R.id.imageView);
-        ProgressBar progress=rootView.findViewById(R.id.progressBar);
+        healthy = 100;
+        ImageView image = (ImageView) rootView.findViewById(R.id.imageView);
+        ProgressBar progress = rootView.findViewById(R.id.progressBar);
 
+        //prefs = getSharedPreferences("Pref", MODE_PRIVATE);
+
+        final GlideDrawableImageViewTarget cd = new GlideDrawableImageViewTarget(image);
         int expireCount = this.getExpiredCount(getContext());
-        progress.setProgress(expireCount*20);
-        if(expireCount <= 2){
-            // 슈다 기분 좋음
-        }
-        else if(expireCount > 2 && expireCount <= 5){
+
+        if (expireCount == 0) {
+            Glide.with(this).load(R.drawable.cdhappy).into(cd);
+            progress.setProgress(healthy);
+        } else if (expireCount > 2 && expireCount <= 4) {
             // 슈다 기분 보통
-        }
-        else{
+            Glide.with(this).load(R.drawable.cdnorm).into(cd);
+        } else if (expireCount > 4 && expireCount <= 6) {
+            // 슈다 기분 보통
+            Glide.with(this).load(R.drawable.cdangry).into(cd);
+        } else {
             // 슈다 화남
+            Glide.with(this).load(R.drawable.cdtired).into(cd);
         }
 
-        final GlideDrawableImageViewTarget cd=new GlideDrawableImageViewTarget(image);
-
-        if(healthy>85) {
+        if (healthy > 85) {
             Glide.with(this).load(R.drawable.cdhappy).into(cd);
         }
 
@@ -71,69 +78,57 @@ public class CharacterFragment extends Fragment {
                 Glide.with(getContext()).load(R.drawable.cdtouch).into(cd);
             }
         });
-
-//
-//        if(progress !=null){
-//            progress.setVisibility(View.VISIBLE);
-//            progress.setIndeterminate(true);
-//            progress.getIndeterminateDrawable().setColorFilter(Color.rgb(10,180,190), PorterDuff.Mode.MULTIPLY);
-//        }
-
-        //http://m.todayhumor.co.kr/view.php?table=programmer&no=13496
-        //progress.getProgressDrawable().setColorFilter(Color.rgb(10,210,200), PorterDuff.Mode.MULTIPLY);
         return rootView;
     }
 
-    public int getExpiredCount(Context context){
+    public int getExpiredCount(Context context) {
         int count = 0;
         Calendar cal = Calendar.getInstance();
         int yearToday = cal.get(cal.YEAR);
         int monthToday = cal.get(cal.MONTH) + 1;
         int dayToday = cal.get(cal.DATE);
-        final DBHelper db = new DBHelper(context,"ITEM.db",null,2);
+        final DBHelper db = new DBHelper(context, "ITEM.db", null, 2);
         String[] foodItem = db.getResult().split("\n");
         String[] Title = new String[db.columnNum()];
         String[] Context = new String[db.columnNum()];
-        for(int i = 0 ; i < db.columnNum(); i++) {
+        for (int i = 0; i < db.columnNum(); i++) {
             String[] str = new String[2];
             str = foodItem[i].split(":");
             Title[i] = str[0].replaceAll("[0-9]", "");
             Context[i] = str[1];
             parseItem(Context[i]);
 
-            if(year < yearToday)
+            if (year < yearToday)
                 count++;
-            else if(month < monthToday)
+            else if (month < monthToday)
                 count++;
-            else if(day < dayToday)
+            else if (day < dayToday)
                 count++;
 
         }
 
         return count;
     }
-    public void parseItem(String item){
+
+    public void parseItem(String item) {
 
         Date today = new Date();
         SimpleDateFormat date = new SimpleDateFormat("yyyy년MM월dd일");
         String d = date.format(today).toString();
 
-        if(item.contains("년")) {
+        if (item.contains("년")) {
             year = Integer.parseInt(item.substring(item.indexOf('년') - 4, item.indexOf('년')));
 
-        }
+        } else
+            year = Integer.parseInt(d.substring(d.indexOf('년') - 4, d.indexOf('년')));
+        if (item.contains("월"))
+            month = Integer.parseInt(item.substring(item.indexOf('월') - 2, item.indexOf('월')).trim());
         else
-            year = Integer.parseInt(d.substring(d.indexOf('년')-4,d.indexOf('년')));
-        if(item.contains("월"))
-            month = Integer.parseInt(item.substring(item.indexOf('월')-2,item.indexOf('월')).trim());
+            month = Integer.parseInt(d.substring(d.indexOf('월') - 2, d.indexOf('월')));
+        if (item.contains("일"))
+            day = Integer.parseInt(item.substring(item.indexOf('일') - 2, item.indexOf('일')).trim());
         else
-            month = Integer.parseInt(d.substring(d.indexOf('월')-2,d.indexOf('월')));
-        if(item.contains("일"))
-            day = Integer.parseInt(item.substring(item.indexOf('일')-2,item.indexOf('일')).trim());
-        else
-            day = Integer.parseInt(d.substring(d.indexOf('일')-2,d.indexOf('일')));
+            day = Integer.parseInt(d.substring(d.indexOf('일') - 2, d.indexOf('일')));
 
     }
-
-
 }
